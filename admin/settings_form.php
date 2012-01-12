5d11626818921c8b846a46293f2e9ffd8c7a3312
Bryan Shelton <bryan@sheltonplace.com>
2012-01-12 15:44:50 -0600

    Converted to using a class

--- admin/settings_form.php
+++ admin/settings_form.php
@@ -1,5 +1,5 @@
 <h2>LocalPath Settings</h2>
-<?php if (!_localpath_enabled()): ?>
+<?php if (!$this->isEnabled()): ?>
 
   <h3 style="color: red;">Localpath has been disabled in wp-config.php</h3>
 
@@ -13,12 +13,12 @@
   <form action="" method="post">
     <input type="hidden" name="action" value="update" id="action" />
     <p><label for="hosts">Hostnames considered local:</label></p>
-    <p><textarea name="hosts" id="hosts" rows="5" cols="40"><?php echo _localpath_opts(); ?></textarea></p>
+    <p><textarea name="hosts" id="hosts" rows="5" cols="40"><?php echo $this->getHostsOption(); ?></textarea></p>
     <p><input type="submit" value="Save options"></p>
   </form>
   <?php else: ?>
   <h3>You have defined your hosts in your wp-config.php</h3>
-  <p style="background-color: #dedfe0; width: 50%; height: 50px; padding: 20px;"><?php echo _localpath_opts(); ?></p>
+  <p style="background-color: #dedfe0; width: 50%; height: 50px; padding: 20px;"><?php echo $this->getHostsOption(); ?></p>
   <?php endif; ?>
 
 <?php endif; ?>
--- localpath.php
+++ localpath.php
@@ -1,150 +1,15 @@
 <?php
 /**
 * @package LocalPath
-* @version 0.2
+* @version 0.3
 */
 /*
 Plugin Name: LocalPath
 Description: Re-write URLs away from admin URLs to the current host.
 Author: University Communications at the University of Wisconsin-Madison
-Version: 0.2
+Version: 0.3
 */
 
-// Register our hooks
-if (_localpath_enabled()) {
-  add_filter('the_content', 'localpath_filter');
-}
-add_action('admin_menu',  'localpath_adminmenu');
-
-/**
- * localpath_filter() does the replacement in the page/post content
- *
- * @param string $content
- *
- * @return string
- *  Returns a filtered string
- */
-function localpath_filter($content) {
-  // Grab our hosts
-  $hosts = _localpath_hosts();
-
-  // Bail if we don't have hosts defined
-  if(empty($hosts)) {
-    return $content;
-  }
-
-  if(!empty($hosts)) {
-    $hosts = array_map('_localpath_preg_prepare', $hosts);
-    $pattern = '~(href|src|HREF|SRC)="((http|https)://(' . implode('|', $hosts) . ')([^"]*))"~x';
-    $content = preg_replace_callback($pattern, 'localpath_replace_callback', $content);
-  }
-
-  return $content;
-}
-
-/**
- * The replace callback for preg_replace_callback
- *
- * @NOTE: We need to determine if we should ditch HTTPS
- *
- * @param array $matches
- *  Provided by preg_replace_callback, this is an array of matches
- * @return string
- *  Returns a string for the replacement
- */
-function localpath_replace_callback($matches) {
-  $out = $matches[1] . '="';
-  $out .= preg_replace('~^(http|https)?://(' . $matches[4] . ')~', get_site_url(), $matches[2]);
-  $out .= '"';
-  return $out;
-}
-
-/**
- * Register the admin page
- */
-function localpath_adminmenu() {
-  add_options_page('LocalPath Options', 'LocalPath Options', 'manage_options', 'localpath_admin', 'localpath_admin');
-}
-
-/**
- * Render the admin page
- */
-function localpath_admin() {
-  // See if we need to handle form submission
-  if(isset($_POST['action']) && $_POST['action'] == "update") {
-    update_option( 'localpath_hosts', trim($_POST['hosts']) );
-    $localpath_notice = "Settings Saved";
-  }
-
-  // Include the form
-  include dirname(__FILE__) . '/admin/settings_form.php';
-  return TRUE;
-}
-
-/**
- * _localpath_opts() - Helper function to grab the localpath WP options
- *
- * This function is safe to call multiple times. It will only hit
- * the database once and will subsequently return the static variable.
- *
- * @return string
- *  Returns the localpath_hosts option from the wp options table.
- */
-function _localpath_opts() {
-  // Make the return a static var so we only pull from the DB once
-  static $localpath_options;
-  if(isset($localpath_options)) { return $localpath_options; }
-
-  // Get the option from the DB or the
-  if (defined('LOCALPATH_HOSTS')) {
-    $localpath_options = trim(LOCALPATH_HOSTS);
-  }
-  else {
-    $localpath_options = trim(get_option('localpath_hosts', ''));
-  }
-
-  return $localpath_options;
-}
-
-/**
- * _localpath_hosts() - Helper function to return an array of hosts
- *
- * @return array
- *  Returns an array of hosts considered local
- */
-function _localpath_hosts() {
-  $opts = _localpath_opts();
-  if(empty($opts)) {
-    return array();
-  }
-  else {
-    $replace = preg_split('/\s/', preg_replace('/\s+/', ' ', $opts));
-    $out = array_map('trim', $replace);
-    return array_map('strtolower', $out);
-  }
-}
-
-/**
- * Function to try to read the LOCALPATH_ENABLED constant
- * to see if the plugin has been disabled in the wp-config.php file
- *
- * @return boolean
- */
-function _localpath_enabled() {
-  return (defined('LOCALPATH_ENABLED') && !LOCALPATH_ENABLED) ? FALSE : TRUE;
-}
-
-/**
- * Clean up each host from _localpath_hosts
- * before being used in the search regular expression
- *
- * @param string
- *  The string to prepare
- *
- * @return string
- *  The preg_quote escaped string with ~ as the delimiter
- */
-function _localpath_preg_prepare($s) {
-  $s = preg_replace('~/+$~', '', $s);
-  return preg_quote($s, "~");
-}
+require_once dirname(__FILE__) . '/lib/localpath.class.php';
+$localpath = new Localpath();
+$localpath->wpInit();
