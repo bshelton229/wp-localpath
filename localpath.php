ab5a5377057f43131e9ae84eea0210df392101f4
Bryan Shelton <bryan@sheltonopensolutions.com>
2012-02-12 15:59:53 -0600

    Template helper function localpath_filter()

--- localpath.php
+++ localpath.php
@@ -13,3 +13,15 @@ Version: 0.3
 require_once dirname(__FILE__) . '/lib/localpath.class.php';
 $localpath = new Localpath();
 $localpath->wpInit();
+
+/**
+ * Helper function for replacing content
+ * @param {String} $content
+ *  The content to replace
+ * @return {String}
+ *  Returns the replaced content
+ */
+function localpath_filter($content) {
+  $localpath = new Localpath();
+  return $localpath->contentFilter($content);
+}
